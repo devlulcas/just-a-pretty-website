5ac8e6267abe3760b00e5b1835ddfe733ce1533e
lucas rego <lucasalvesrego.contato@gmail.com>
2023-07-20 19:18:43 -0300

    feat: better seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://just-a-pretty-website.vercel.app',
+  integrations: [sitemap()]
+});
